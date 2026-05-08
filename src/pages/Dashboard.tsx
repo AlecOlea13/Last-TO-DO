3ac80ae293905d4c1e796b111941942fca1a6f18
AlecOlea13 <2122100694@soy.utj.edu.mx>
2026-05-07 21:08:00 -0600

    outlet cambiau

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -181,7 +181,7 @@ export default function Dashboard() {
             </div>
           </>
         ) : (
-          <Outlet />
+          <Outlet context={{ user }} />
         )}
       </div>
     </div>
