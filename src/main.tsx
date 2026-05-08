9a62abfc48c5437315adc820e297f777ddbb159b
AlecOlea13 <2122100694@soy.utj.edu.mx>
2026-05-08 09:20:03 -0600

    cambio de main

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login        from "./pages/Login";
-import Register     from "./pages/Register";
-import Dashboard    from "./pages/Dashboard";
-import Profile      from "./pages/Profile";
-import VerifyEmail  from "./pages/VerifyEmail"; // ← agregar
-import ProtectedRoute from "./routes/ProtectedRoute";
-
 import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"             element={<Login />} />
-        <Route path="/register"     element={<Register />} />
-        <Route path="/verify-email" element={<VerifyEmail />} /> {/* ← agregar */}
-
-        {/* Dashboard protegido */}
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>}>
-          <Route path="profile" element={<Profile />} />
-          <Route path="*" element={<Navigate to="profile" replace />} />
-        </Route>
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
