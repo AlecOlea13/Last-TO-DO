c68ddb68bc3b236d0cd7eac7c9f18523158d97d2
Alec <2122100694@soy.utj.edu.mx>
2026-07-24 13:45:02 -0600

    mas fixes

--- src/pages/CxC.tsx
+++ src/pages/CxC.tsx
@@ -324,6 +324,9 @@ export default function CuentasCobrar() {
       if (conceptosRenta.length > 0 && formF.rfcReceptor) {
         try {
           const { data: rentaData } = await api.post("/rentas/buscar-por-rfc", { rfc: formF.rfcReceptor });
+          //ojito con borrar el console log
+          console.log("RENTAS:", rentaData.rentas.map((r: any) => r.montacargas?.numeroEconomico));
+          console.log("EXTRAIDOS:", conceptosRenta.map(c => extraerNumeroEconomico(c.descripcion)));
           if (rentaData.rentas?.length > 0) {
             const rentas: RentaDetectada[] = rentaData.rentas;
             const items: RenovacionItem[] = conceptosRenta.map(c => {
