5d3a53998d6da3acbbfd6866f909a3cd4ab4b166
Alec <2122100694@soy.utj.edu.mx>
2026-08-12 12:31:30 -0600

    Pipsy bien?

--- src/components/IAWidget.tsx
+++ src/components/IAWidget.tsx
@@ -31,7 +31,7 @@ interface Resumen {
   };
 }
 
-const API_BACK = import.meta.env.VITE_API_URL ?? "https://pipsa-back.vercel.app";
+const API_BACK = (import.meta.env.VITE_API_URL ?? "https://pipsa-back.vercel.app").replace(/\/api$/, "");
 
 function formatMXN(n: number) {
   return new Intl.NumberFormat("es-MX", { style: "currency", currency: "MXN" }).format(n);
